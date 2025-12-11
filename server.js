17c7d75acae7958d99dc1a3fc03d835b561c8223
vitempolo <thugyi1333@gmail.com>
2025-12-11 21:07:18 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(path.join(__dirname)));
+
+io.on('connection', socket => {
+  console.log('New client connected');
+
+  socket.on('signal', data => io.emit('signal', data));
+  socket.on('chat', msg => io.emit('chat', msg));
+  socket.on('file', file => io.emit('file', file));
+
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+server.listen(3000, () => console.log('Server running on http://localhost:3000'));
